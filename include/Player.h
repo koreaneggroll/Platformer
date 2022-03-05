2463a225397990d097d81facc35292d5bc440c3d
koreaneggroll <aringrigoras@gmail.com>
2022-03-05 22:10:46 +0200

    player class

---
+++ include/Player.h
@@ -0,0 +1,16 @@
+#ifndef PLAYER_H
+#define PLAYER_H
+
+
+class Player
+{
+    private:
+
+
+    public:
+    
+        Player();
+        virtual ~Player();
+};
+
+#endif //PLAYER_H
---
+++ src/Player.cpp
@@ -0,0 +1,13 @@
+#include "../include/Player.h"
+
+
+Player::Player()
+{
+
+}
+
+
+Player::~Player()
+{
+    
+}
