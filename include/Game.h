508e2189f64404dbf64e9a60e948e1ea8f173ae5
koreaneggroll <aringrigoras@gmail.com>
2022-03-05 21:31:47 +0200

    game class

---
+++ include/Game.h
@@ -0,0 +1,19 @@
+#ifndef GAME_H
+#define GAME_H
+
+
+#include "../precomp/stdafx.h"
+
+
+class Game
+{
+    private:
+    
+
+    public:
+        Game();
+        virtual ~Game();
+};
+
+
+#endif //GAME_H
